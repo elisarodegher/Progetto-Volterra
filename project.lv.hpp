8de2781eacd1dc423e3d9542ae9cf67723199cd9
francesco-masi <masifrancesco04@gmail.com>
2026-01-29 17:26:58 +0100

    commenti sull'header

--- project.lv.hpp
+++ project.lv.hpp
@@ -7,20 +7,21 @@
 
 namespace lv {
 
-struct Parameters {
-  double A;  // prey birth rate
-  double B;  // prey death rate
-  double C;  // predator birth rate
-  double D;  // predator death rate
+struct Parameters {  // contiene i parametri (coefficienti) della simulazione.
+  double A;          // prey birth rate
+  double B;          // prey death rate
+  double C;          // predator birth rate
+  double D;          // predator death rate
 };
 
-struct State {
+struct State {  // Questa struct contiene le variabili dell'eq. diff: numero di
+                // prede, predatori e integrale primo
   double prey;
   double predator;
   double H;
 };
 
-struct Statistics {
+struct Statistics {  // struct dei risultati, ovvero delle statistiche.
   double mean;
   double sigma;
   double maximum;
@@ -28,47 +29,53 @@ struct Statistics {
 };
 
 class Simulation {
-  Parameters const par_;
-  double const dt_;
-  std::size_t const iterations_;
-  State state_;
-  std::vector<State> evolution_;
-  Statistics prey_stat_;
-  Statistics pred_stat_;
+  Parameters const par_;          // parametri della simulazione
+  double const dt_;               // passo dt temporale
+  std::size_t const iterations_;  // numero di interazioni, size_t è solo un
+                                  // altro nome per unsigned long
+  State state_;                   // stato istantaneo della simulazione
+  std::vector<State> evolution_;  // vettore con tutti gli stati in fila
+  Statistics prey_stat_;          // statistiche della preda
+  Statistics pred_stat_;          // statistiche del predatore
 
-  explicit Simulation(Parameters par, double prey, double pred, double dt, std::size_t it)  // MAIN CONSTRUCTOR
+  explicit Simulation(Parameters par, double prey, double pred, double dt,
+                      std::size_t it)  // Costruttore principale di Simulation
       : par_(par), dt_(dt), iterations_(it) {
-    // DEFINING INITIAL STATE
-    state_.H = (par_.C * prey) + (par_.B * pred) - ((par_.D * std::log(prey)) + (par_.A * std::log(pred)));
+    // Calcolo di H iniziale:
+    state_.H = (par_.C * prey) + (par_.B * pred) -
+               ((par_.D * std::log(prey)) + (par_.A * std::log(pred)));
 
-    evolution_.push_back(State{prey, pred, state_.H}); // vettore che contiene gli stati del sistema messi in ordine cronologico 
+    evolution_.push_back(
+        State{prey, pred, state_.H});  // vettore che contiene gli stati del
+                                       // sistema messi in ordine cronologico
 
-    // SWITCHING TO RELATIVE COORDINATES FOR COMPUTATIONS
+    // Cambio di coordinate (H è invariante al cambio di sdr)
     state_.prey = prey * par_.C / par_.D;
     state_.predator = pred * par_.B / par_.A;
   }
-// sono le funzioni che preparano la simulazione
+  // sono le funzioni che preparano la simulazione
   static Simulation set_simulation();
-
   static Simulation set_simulation(std::string const& file_name);
-// funzioni per controllare la validità dei parametri
+
+  // funzioni per controllare la validità dei parametri
   static Parameters const& is_good(Parameters const& p);
-
   static double is_good(double val);
-
   static double dt_good(double t);
-
   static std::size_t it_good(double val);
 
  public:
-  // DELEGATING CONSTRUCTORS
+  // Costruttore pubblico
   Simulation(Parameters p, double prey, double pred, double t, double n)
-      : Simulation(is_good(p), is_good(prey), is_good(pred), dt_good(t), it_good(n)) {}
+      : Simulation(is_good(p), is_good(prey), is_good(pred), dt_good(t),
+                   it_good(n)) {}
 
+  // Creazione di una simulazione leggendo i dati dall'utente
   Simulation() : Simulation(set_simulation()) {}
 
+  // Creazione di una simulazione leggendo un file
   Simulation(std::string const& name) : Simulation(set_simulation(name)) {}
 
+  // Funzioni di lettura di parametri
   Parameters const& parameters() const { return par_; }
 
   State const& init_state() const { return evolution_.front(); }
@@ -87,11 +94,15 @@ class Simulation {
 
   Statistics const& pred_stat() { return pred_stat_; }
 
-  void evolve();
+  // Funzioni di calcolo
 
-  void compute();
+  void evolve();  // fa evolvere la simulazione
 
-  void statistics();
+  void compute();  // fa i calcoli
+
+  void statistics();  // ritorna le statistiche
+
+  // funzioni di presentazione dei risultati
 
   void show_evolution(int p) const;
 
@@ -106,18 +117,19 @@ class Simulation {
   void save_statistics(int p, std::string const& name);
 };
 
-// ###################### OPERATORS
+// Definizione di operatori
 
 bool operator==(Parameters const& a, Parameters const& b);
 
 bool operator==(State const& a, State const& b);
 
-// ###################### FREE FUNCTIONS
+// Funzioni fuori dalla classe (libere)
 
 bool comp_prey(State const& a, State const& b);
 
 bool comp_pred(State const& a, State const& b);
 
 bool read(std::ifstream& in, std::string& str, char const delimiter);
+
 }  // namespace lv
 #endif

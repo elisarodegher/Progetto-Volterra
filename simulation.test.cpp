36d1d486966e43c93e2e4fa13e6de545cacd9c1c
elisarodegher <elisarodegher04@gmail.com>
2026-02-04 19:11:04 +0100

    alcuni fix

--- simulation.test.cpp
+++ simulation.test.cpp
@@ -83,38 +83,36 @@ TEST_CASE("Testing constructors' throws") {
   }
 }
 
-TEST_CASE("Testing object construction") {
-  volterra::Simulation sim({500., 2., 4., 1000.}, 1000., 250., 0.0002, 4.8);
 
-  CHECK(sim.parameters() == volterra::Parameters{500., 2., 4., 1000.});
-  CHECK(sim.initial_state() == volterra::State{1000., 250., 0.});
-  CHECK(sim.internal_state() == volterra::State{4., 1., 0.});
-  CHECK(sim.timescale() == 0.0002);
-  CHECK(sim.iterations() == std::size_t(4));
+TEST_CASE("Testing object construction") {
+  volterra::Simulation sim({800., 1., 1., 1000.}, 2000., 2000., 0.0001, 3.5);
+
+  CHECK(sim.parameters() == volterra::Parameters{800., 1., 1., 1000.});
+  CHECK(sim.initial_state() == volterra::State{2000., 2000., 0.});
+  CHECK(sim.internal_state() == volterra::State{2., 2.5, 0.});
+  CHECK(sim.timescale() == 0.0001);
+  CHECK(sim.iterations() == std::size_t(3));
 }
 
 TEST_CASE("Testing sim") {
-  volterra::Simulation sim({500., 2., 4., 1000.}, 1000., 250., 0.0002, 4.8);
+  volterra::Simulation sim({800., 1., 1., 1000.}, 2000., 2000., 0.0001, 3.);
 
   SUBCASE("Testing evolve()") {
     sim.evolve();
 
-    CHECK(sim.current_state().x == doctest::Approx(1000.).epsilon(1e-12));
-    CHECK(sim.current_state().y == doctest::Approx(400.).epsilon(1e-12));
-    CHECK(sim.internal_state().x == doctest::Approx(4.).epsilon(1e-12));
-    CHECK(sim.internal_state().y == doctest::Approx(1.6).epsilon(1e-12));
+    CHECK(sim.current_state() == volterra::State{1760., 2200., 0.});
+    CHECK(sim.internal_state() == volterra::State{1.76, 2.75, 0.});
   }
 
   SUBCASE("Testing go()") {
     sim.go();
 
     auto ev = sim.evolution();
-    REQUIRE(ev.size() == std::size_t(4));
 
-    std::vector<volterra::State> vec{{1000., 250., 0.},
-                                     {1000., 400., 0.},
-                                     {1000., 640., 0.},
-                                     {1000., 1024., 0.}};
+    REQUIRE(ev.size() == std::size_t(3));
+
+    std::vector<volterra::State> vec{
+        {2000., 2000., 0.}, {1760., 2200., 0.}, {1513.6, 2367.2, 0.}};
 
     for (std::size_t i{0}; i < ev.size(); ++i) {
       CHECK(ev[i].x == doctest::Approx(vec[i].x).epsilon(0.0001));

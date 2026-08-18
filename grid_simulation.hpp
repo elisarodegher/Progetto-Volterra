73882483979c05c01d11cc84833dac914799c7b3
elisarodegher <elisarodegher04@gmail.com>
2026-08-18 16:45:58 +0200

    pulizia commenti

--- grid_simulation.hpp
+++ grid_simulation.hpp
@@ -4,7 +4,7 @@
 
 namespace volterra {
 
-// STRUCT
+// ----------------------------------------------STRUCT-------------------------------------------------------------------
 enum class CellState { Empty, Prey, Predator };
 
 struct Cell {
@@ -18,7 +18,7 @@ struct Population {
   size_t sharks;  // predators = sharks
 };
 
-// CLASSE
+//------------------------------------------ CLASSE--------------------------------------------------------------------
 class GridSimulation {
   Parameters parameters_;  // preso da simulation.hpp
   int width_;              // larghezza griglia
@@ -30,15 +30,19 @@ class GridSimulation {
       history_;       // vettore che contiene il registro dei vari stati della
                       // popolazione, contiene le struct populations
   std::mt19937 rng_;  // generatore di numeri casuali
-  // index calculations
+
+
+
+  // ----------------------------------------index calculations---------------------------------------------------------
   size_t index(int row, int col) const { return row * width_ + col; }
 
   Cell& current_cell(int row, int column) {
     return grid_[index(row, column)];
   };  // probabilmente da sistemare per conversioni implicite
 
-  // changing cells with pacman effect; reminder: higher rows have lower
-  // indexes!
+
+  // ---------------------------------------changing cells with pacman effect ---------------------------------------------------------
+  // reminder: higher rows have lower indexes!
   std::size_t up(std::size_t row) const {
     return (row + height_ - 1) % height_;
   }
@@ -49,22 +53,27 @@ class GridSimulation {
   std::size_t right(std::size_t col) const { return (col + 1) % width_; }
 
  public:
+
+ // ----------------------------------------------------costruttore-----------------------------------------------------------
+
   GridSimulation(Parameters p, int width, int height, double prey_density,
                  double predator_density, int iterations);
 
-  // getters
+
+  // ------------------------------------------------getters----------------------------------------------------------------
   Parameters parameters() const { return parameters_; }
   int width() const { return width_; }
   int height() const { return height_; }
   int iterations() const { return iterations_; }
   std::vector<Population> const& history() const { return history_; }
 
-  // evolution
+  // --------------------------------------------evolution----------------------------------------------------------------
   void evolve();
   void go();
 };
 
-// operatori di confronto
+
+//---------------------------------------------------- operatori di confronto---------------------------------------------------
 bool operator==(Cell const& a, Cell const& b) {
   return a.state == b.state && a.energy == b.energy && a.age == b.age;
 }
